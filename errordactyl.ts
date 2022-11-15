7919a376ed0a155a205f61002ce6dbd19b1c1888
Eric Vartanian <eric.vartanian1@gmail.com>
2022-11-14 17:52:55 -0800

    added new option for file parsing

--- errordactyl.ts
+++ errordactyl.ts
@@ -15,7 +15,8 @@ const methods: methodCache = {
     delete: [],
     patch: []
   }
-  
+
+// // better time complexity option
 for (const method in methods) {
     const regex = new RegExp(`\\.${method}\\("(\\S)*"`, 'g');
     const endpoints: RegExpMatchArray|null = file.match(regex);
@@ -27,3 +28,20 @@ for (const method in methods) {
 }
 
 console.log(methods);
+
+// better space complexity option
+import { readLines } from 'https://deno.land/std@0.78.0/io/mod.ts';
+const file1 = await Deno.open('./router.ts');
+for await (const line of readLines(file1)) {
+  for (const method in methods) {
+    const regex = new RegExp(`(?<=\\.${method}\\()\\'(.*)\\'`, 'g');
+    const found = line.match(regex);
+    if (found) {
+      const path = found[0].substring(1, found[0].length - 1);
+      methods[method as keyof typeof methods].push(path);
+    }
+  }
+}
+
+console.log(methods);
+
