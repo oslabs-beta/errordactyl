29c32bf7712f37d6839f0685e5fc06d3e97c5136
Lewis Lin <linle211@gmail.com>
2022-11-23 18:03:28 -0800

    New Github Name

--- server.ts
+++ server.ts
@@ -2,6 +2,8 @@ import { Application } from './deps.ts';
 import router from './router.ts';
 import errors from './error.ts';
 
+console.log("new branch");
+
 const app = new Application( {logErrors: false} );
 
 app.addEventListener('error', (evt) => {errors(evt.error)})
