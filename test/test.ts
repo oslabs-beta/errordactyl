df8302d5dfea19ab9ca8fe5651a0f9d5d5ffa1e3
Tony Van <thetonyvan@gmail.com>
2022-12-02 21:23:17 -0500

    added code for single endpoint testing

--- test/test.ts
+++ test/test.ts
@@ -1,18 +1,20 @@
 import { endpoint } from '../types.ts';
 
+const td = (d: Uint8Array) => new TextDecoder().decode(d);
+
+const config = await Deno.readTextFile('./_errordactyl/config.json').then(res => JSON.parse(res));
+
+const pathToServer:string = config.serverPath;
+
+let script = `#!/bin/bash\ndeno run --allow-net ${pathToServer} &\nDENO_PID=$!\nsleep .5`;
+
 export async function test() {
-  const td = (d: Uint8Array) => new TextDecoder().decode(d);
-  
-  const config = await Deno.readTextFile('./_errordactyl/config.json').then(res => JSON.parse(res));
-  
-  const pathToServer:string = config.serverPath;
-  
-  let script = `#!/bin/bash\ndeno run --allow-net ${pathToServer} &\nDENO_PID=$!\nsleep .5`;
-  
   for (const method in config.endpoints) {
     addRoutes(method, config.endpoints[method]);
   }
-  
+
+  script += `\nkill $DENO_PID`;
+
   function addRoutes(method:string, arr:Array<endpoint>) {
     switch (method) {
       case 'GET':
@@ -64,12 +66,6 @@ export async function test() {
     }
   }
   
-  script += `\nkill $DENO_PID`;
-  
-  async function writeFile(path:string, text:string): Promise<void> {
-    return await Deno.writeTextFile(path, text);
-  }
-  
   await writeFile('./_errordactyl/test.sh', script);
   
   await Deno.run({cmd: ['chmod', '+x', './_errordactyl/test.sh']}).status();
@@ -80,7 +76,58 @@ export async function test() {
   console.log('Your server responded:');
   console.log(td(await p.output()).trim())
   console.log('STDERR:', td(await p.stderrOutput()).trim());
+}
 
+export async function testOne(method:string, endpoint:string, body?:string) {
+  switch (method) {
+    case 'GET':
+      script += `
+      \nGET=$(curl -s localhost:3000${endpoint})
+      echo "GET to '${endpoint}': \$GET"
+      `
+    break;
+    case 'POST':
+      script += `
+      \nPOST=$(curl -s -X POST -d '${body}' localhost:3000${endpoint})
+      echo "POST to '${endpoint}': \$POST"
+      `
+    break;
+    case 'PATCH':
+      script += `
+      \nPATCH=$(curl -s -X PATCH -d '${JSON.stringify(body)}' localhost:3000${endpoint})
+      echo "PATCH to '${endpoint}': \$PATCH"
+      `
+    break;
+    case 'PUT':
+      script += `
+      \nPATCH=$(curl -s -X PATCH -d '${JSON.stringify(body)}' localhost:3000${endpoint})
+      echo "PATCH to '${endpoint}': \$PATCH"
+      `
+    break;
+    case 'DELETE':
+      script += `
+      \nDELETE=$(curl -s -X DELETE localhost:3000${endpoint})
+      echo "DELETE to '${endpoint}': \$DELETE"
+      `
+    break;
+  }
+
+  script += `\nkill $DENO_PID`;
+
+  await writeFile('./_errordactyl/test.sh', script);
+  
+  await Deno.run({cmd: ['chmod', '+x', './_errordactyl/test.sh']}).status();
+  
+  const p = await Deno.run({cmd: ['./_errordactyl/test.sh'], stdout:'piped', stderr:'piped'});
+  
+  console.log(await p.status());
+  console.log('Your server responded:');
+  console.log(td(await p.output()).trim())
+  console.log('STDERR:', td(await p.stderrOutput()).trim());
+}
+
+async function writeFile(path:string, text:string): Promise<void> {
+  return await Deno.writeTextFile(path, text);
 }
 
 
