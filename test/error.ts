dd3f3382dde1a5a85cc8319b8e3a510275f64cb1
Lewis Lin <linle211@gmail.com>
2022-12-02 18:35:58 -0800

    Error Function 1.0 Complete

--- test/error.ts
+++ test/error.ts
@@ -1,25 +1,73 @@
 const allErrors = {...Deno.errors, EvalError, RangeError, ReferenceError, SyntaxError, TypeError, URIError, AggregateError, Error}
 
-export default function errors(error): [] | void {
+// export default function errors(error): [] | void {
 
-  for (const errorType in allErrors) {
-    if (error instanceof allErrors[errorType]) {
+//   for (const errorType in allErrors) {
+//     if (error instanceof allErrors[errorType]) {
 
-      const arrayErrorStack = error.stack.split("\n    at ");
-      arrayErrorStack.error = {};
+//       const arrayErrorStack = error.stack.split("\n    at ");
+//       arrayErrorStack.error = {};
+//       const regex = /(?!:)\d+/g;
+//       const regex2 = /(?<=file:\/\/)\S*/;
+
+//       [arrayErrorStack.error.message, arrayErrorStack.error.location] = arrayErrorStack;
+//       arrayErrorStack.error.location = arrayErrorStack.error.location.match(regex2)[0];
+//       [arrayErrorStack.error.lineNo, arrayErrorStack.error.colNo] = arrayErrorStack.error.location.match(regex);
+
+//       // console.log(arrayErrorStack)
+//       console.log(arrayErrorStack.error)
+
+//       return arrayErrorStack;
+//     }
+//   }
+
+//   // throw new Error("Undetected Error Type");
+// }
+
+export default function errors(errorMessage): [] | void {
+
+  // console.log(errorMessage)
+
+      const arrayErrorStackSplit = (JSON.stringify(errorMessage)).split("\\n\\n");
+      const arrayErrorStack = [];
+      // console.log("Array Error Stack Split", arrayErrorStackSplit)
+
+      arrayErrorStackSplit.forEach((stringElement) => {
+        stringElement.split('\\n').forEach((element) => arrayErrorStack.push(element.replace('    at ','')))}
+      )
+
+      console.log("Array Error Stack", arrayErrorStack)
+
       const regex = /(?!:)\d+/g;
-      const regex2 = /(?<=file:\/\/)\S*/;
+      const regex2 = /(?<=file:\/\/)\S*/;  // looking for strings after file://...
+      const regex3 = /(?<=\.(j|t)s:)\S*/;  // looking for .js or .ts suffixes
+      const regex4 = /method: \\"(.*)\\"/
 
-      [arrayErrorStack.error.message, arrayErrorStack.error.location] = arrayErrorStack;
-      arrayErrorStack.error.location = arrayErrorStack.error.location.match(regex2)[0];
-      [arrayErrorStack.error.lineNo, arrayErrorStack.error.colNo] = arrayErrorStack.error.location.match(regex);
+      const arrayOfUsefulErrorInformation = [];
 
-      // console.log(arrayErrorStack)
-      console.log(arrayErrorStack.error)
+      for (let i = 0; i < arrayErrorStack.length; i++) {
+        if (arrayErrorStack[i].includes('[uncaught application error]:')) {
+          const strobj = {
+            message: 'null',
+            request: 'null',
+            response: 'null',
+            location: 'null',
+            lineNo: 'null',
+            colNo: 'null',
+            http: 'null',
+          };
 
-      return arrayErrorStack;
-    }
-  }
+          [strobj.message, strobj.request, strobj.response, strobj.location] = [arrayErrorStack[i], arrayErrorStack[i+1], arrayErrorStack[i+2], arrayErrorStack[i+3]];
+          strobj.location = strobj.location.match(regex2)[0];
+          [strobj.lineNo, strobj.colNo] = strobj.location.match(regex3)[0].split(':');
+          strobj.http = strobj.request.match(regex4)[1];
 
-  // throw new Error("Undetected Error Type");
+          arrayOfUsefulErrorInformation.push(strobj);
+        }
+      }
+
+
+
+      return arrayOfUsefulErrorInformation;
+
 }
