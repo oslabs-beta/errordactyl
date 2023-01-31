cf4c3bbddb7c514d3a9eef3a1295521f547736b5
Lewis Lin <linle211@gmail.com>
2023-01-30 19:39:25 -0800

    Node Migration

--- test/class.ts
+++ test/class.ts
@@ -21,7 +21,7 @@ export default class Test {
     this.config = await fs.readFile('./_errordactyl/config.json').then(res => JSON.parse(res)); // fs.readFile
     const pathToServer:string = this.config.serverPath;
     // this.script = `#!/bin/bash\ndeno run --allow-net ${pathToServer} &\nDENO_PID=$!\nsleep .5\n`; // change CLI script command to use node
-    this.script = `#!/bin/bash\nnode ${pathToServer} &\nDENO_PID=$!\nsleep .5\n`; // change CLI script command to use node
+    this.script = `#!/bin/bash\necho 'i am inside of the shell'\nnode ${pathToServer} &\nDENO_PID=$!\nsleep .5\necho 'i am also inside of the shell'\n`; // change CLI script command to use node
     const colorVars =
     `NC='\\0033[0m'
     BPURPLE='\\033[1;35m'
@@ -63,25 +63,29 @@ export default class Test {
   }
 
   writeAndRun = async () => {
+    console.log('writeAndRun Method invoked');
     this.script += `\nkill $DENO_PID`; // Change to Node's specific version of PID (unknown)
 
     // await Deno.writeTextFile('./_errordactyl/test.sh', this.script);
     await fs.writeFile('./_errordactyl/test.sh', this.script); // fs.writeFile
 
     // await Deno.run({cmd: ['chmod', '+x', './_errordactyl/test.sh']}).status(); // figure out Node subprocess and re-wrtie
+    // console.log('first subprocess');
     await spawn('chmod', ['+x', './_errordactyl/test.sh']); // figure out Node subprocess and re-write
 
     // const p = await Deno.run({cmd: ['./_errordactyl/test.sh'], stdout:'piped', stderr:'piped'}); // figure out Node subprocess and re-write
+    // console.log('second subprocess');
     const p = await spawn('./_errordactyl/test.sh', [], {stdio: 'pipe'})
     console.log('%cYour server responded:%c\n', 'background-color: white', 'background-color: transparent');
 
-    p.stdout.on('data', (data) => {
-      console.log(this.td(data).trim())
+    p.stdout.on('data0', (data) => {
+      console.log('data1', this.td(data).trim())
     });
 
     // const STDERR = (this.td(await p.stderrOutput()).trim())
-    p.stderr.on('data', (data) => {
-      console.log(errors(this.td(data).trim())); // <---------------- !!!!
+    p.stderr.on('errorData0', (data) => {
+      console.log('errorData1', data);
+      console.log('errorData2', errors(this.td(data).trim())); // <---------------- !!!!
     })
   }
 
